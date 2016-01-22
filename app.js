9f339c96c1dc883224ae6a590f3678aa156ef513
Peter Parente <pparent@us.ibm.com>
2016-01-22 10:02:02 -0500

    Move more config to nconf
    (c) Copyright IBM Corp. 2016

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 
 var hbsHelpers = require('./app/handlebars-helpers');
+var config = require('./app/config');
 
 var routes = require('./routes/index');
 var apiRoutes = require('./routes/api');
 
 var app = express();
 
-var env = process.env.NODE_ENV || 'development';
+var env = config.get('NODE_ENV') || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
