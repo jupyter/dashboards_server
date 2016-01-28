3bc18f298a879a7575861ba3e2d7f4b47ffb2345
Justin Tyberg <jtyberg@us.ibm.com>
2016-01-28 18:11:24 -0500

    [Issue: 25] NodeJS app should send kernel shutdown when client websocket disconnects
    Kill kernel on kernel gateway if client socket connection dies.
    
    (c) Copyright IBM Corp. 2016

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
  * Copyright (c) Jupyter Development Team.
  * Distributed under the terms of the Modified BSD License.
  */
+var http = require('http');
 var httpProxy = require('http-proxy');
 var debug = require('debug')('dashboard-proxy:server');
 var wsutils = require('../app/ws-utils');
@@ -17,87 +18,121 @@ var kgBaseUrl = config.get('KG_BASE_URL');
 
 var server = null;
 var sessions = {}; // TODO remove old sessions, somehow
+var apiRe = new RegExp('^/api(/.*$)');
+var kernelIdRe = new RegExp('^.*/kernels/([^/]*)');
 
 var proxy = httpProxy.createProxyServer({
         target: urljoin(kgUrl, kgBaseUrl, '/api')
     });
+
+var substituteCodeCell = function(d) {
+    debug('PROXY: received message from client WS: ' + (d && d.payload));
+    var transformedData = d;
+
+    // substitute in code if necessary
+    // for performance reasons, first do a quick string check before JSON parsing
+    if (d && d.payload.indexOf('execute_request') !== -1) {
+        try {
+            d.payload = JSON.parse(d.payload);
+            if (d.payload.header.msg_type === 'execute_request') {
+                // get notebook data for current session
+                var nbpath = sessions[d.payload.header.session];
+                transformedData = nbstore.get(nbpath).then(
+                  function success(nb) {
+                        // get code string for cell at index and update WS message
+
+                        // code must be an integer corresponding to a cell index
+                        var cellIdx = parseInt(d.payload.content.code, 10);
+
+                        // code must only consist of a non-negative integer
+                        if (cellIdx.toString(10) === d.payload.content.code &&
+                                cellIdx >= 0) {
+
+                            // substitute cell's actual code into the message
+                            var code = nb.cells[cellIdx].source.join('');
+                            d.payload.content.code = code;
+                            d.payload = JSON.stringify(d.payload);
+                        } else {
+                            // throw away execute request that has non-integer code
+                            d = null;
+                        }
+                        return d;
+                    },
+                    function error() {
+                        return d; // data remains unchanged
+                    });
+            }
+        } catch(e) {
+            // TODO better handle parse error in WS message
+            console.error('Failed to parse `data` in WS. Leaving unchanged.', e);
+        }
+    }
+    return transformedData;
+};
 
 function setupWSProxy(_server) {
     debug('setting up WebSocket proxy');
     server = _server;
 
     // Listen to the `upgrade` event and proxy the WebSocket requests as well.
-    var re = new RegExp('^/api(/.*$)');
     _server.on('upgrade', function(req, socket, head) {
         var _emit = socket.emit;
         socket.emit = function(eventName, data) {
-            var codeCellsSubstituted = data;
             if (eventName === 'data') {
+                var codeCellsSubstituted = data;
                 var decodedData = wsutils.decodeWebSocket(data);
 
                 // decodedData is an array of multiple messages
-                codeCellsSubstituted = decodedData.map(function(d) {
-                    debug('PROXY: received message from client WS: ' + (d && d.payload));
-                    var transformedData = d;
+                codeCellsSubstituted = decodedData.map(substituteCodeCell);
 
-                    // substitute in code if necessary
-                    // for performance reasons, first do a quick string check before JSON parsing
-                    if (d && d.payload.indexOf('execute_request') !== -1) {
-                        try {
-                            d.payload = JSON.parse(d.payload);
-                            if (d.payload.header.msg_type === 'execute_request') {
-                                // get notebook data for current session
-                                var nbpath = sessions[d.payload.header.session];
-                                transformedData = nbstore.get(nbpath).then(
-                                    function success(nb) {
-                                        // get code string for cell at index and update WS message
-
-                                        // code must be an integer corresponding to a cell index
-                                        var cellIdx = parseInt(d.payload.content.code, 10);
-
-                                        // code must only consist of a non-negative integer
-                                        if (cellIdx.toString(10) === d.payload.content.code &&
-                                                cellIdx >= 0) {
-
-                                            // substitute cell's actual code into the message
-                                            var code = nb.cells[cellIdx].source.join('');
-                                            d.payload.content.code = code;
-                                            d.payload = JSON.stringify(d.payload);
-                                        } else {
-                                            // throw away execute request that has non-integer code
-                                            d = null;
-                                        }
-                                        return d;
-                                    },
-                                    function error() {
-                                        return d; // data remains unchanged
-                                    });
-                            }
-                        } catch(e) {
-                            // TODO better handle parse error in WS message
-                            console.error('Failed to parse `data` in WS. Leaving unchanged.', e);
-                        }
-                    }
-                    return transformedData;
+                Promise.all(codeCellsSubstituted).then(function(data) {
+                    // data is an array of messages
+                    // filter out null messages (if any)
+                    data = data.filter(function(d) {
+                        return !!d;
+                    });
+                    // re-encode
+                    data = wsutils.encodeWebSocket(data);
+                    _emit.call(socket, eventName, data);
                 });
+            } else {
+              _emit.call(socket, eventName, data);
             }
-            Promise.all(codeCellsSubstituted).then(function(data) {
-                // data is an array of messages
-                // filter out null messages (if any)
-                data = data.filter(function(d) {
-                    return !!d;
-                });
-                // re-encode
-                data = wsutils.encodeWebSocket(data);
-                _emit.call(socket, eventName, data);
-            });
         };
 
+        // Add handler for reaping a kernel if the client socket closes.
+        // Extract kernel ID from request url:
+        //    /api/kernels/008589cb-7b81-48d4-abd8-faef1b3257f9?channel....
+        var kernelIdMatched = kernelIdRe.exec(req.url);
+        if (kernelIdMatched) {
+            var kernelId = kernelIdMatched[1];
+            socket.on('close', function () {
+              debug('PROXY: Ws closed for ' + kernelId);
+              killKernel(kernelId);
+            });
+        }
+
         // remove '/api', otherwise proxies to '/api/api/...'
-        req.url = re.exec(req.url)[1];
+        req.url = apiRe.exec(req.url)[1];
         proxy.ws(req, socket, head);
     });
 }
+
+// Kill kernel on backend kernel gateway.
+var killKernel = function(kernelId) {
+    debug('PROXY: killing kernel ' + kernelId);
+    var endpoint = urljoin(kgUrl, kgBaseUrl, '/api/kernels/', kernelId);
+    var headers = {};
+    if (kgAuthToken) {
+        headers['Authorization'] = 'token ' + kgAuthToken;
+    }
+    var req = http.request(endpoint, function(res) {
+        debug('PROXY: kill kernel response: ' + res.statusCode + ' ' + res.statusMessage);
+    });
+    req.headers = headers;
+    req.method = 'DELETE';
+    req.end();
+};
 
 var proxyRoute = function(req, res, next) {
     proxy.web(req, res);
@@ -159,7 +194,7 @@ proxy.on('error', function(err, req, res) {
     // });
 // });
 
-proxy.on('close', function (req, socket, head) {
+proxy.on('close', function (proxyRes, proxySocket, proxyHead) {
     // view disconnected websocket connections
     debug('PROXY: WS client disconnected');
 });
