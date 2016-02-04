47f55b72a2b2a17330d08c73ed40beec01608a01
Peter Parente <pparent@us.ibm.com>
2016-02-04 17:08:12 -0500

    Make plot update properly
    * Lots of code comments
    * Workaround https://github.com/ipython/ipywidgets/pull/353
    * Associate output area models with widgets
    
    (c) Copyright IBM Corp. 2016

--- public/js/widget-manager.js
+++ public/js/widget-manager.js
@@ -7,12 +7,13 @@
 //   https://github.com/ipython/ipywidgets/blob/fc6844f8210761ff5ad1c9ffc25a70b379fc5191/examples/development/web3/src/manager.js
 define(['jupyter-js-widgets'], function(Widgets) {
 
-    var WidgetManager  = function(kernel) {
+    var WidgetManager  = function(kernel, msgHandler) {
         //  Call the base class.
         Widgets.ManagerBase.call(this);
 
-        this._msg_cells = {};
+        this._pendingExecutions = {};
         this.kernel = kernel;
+        this.msgHandler = msgHandler;
 
         // Create a comm manager shim
         this.commManager = new Widgets.shims.services.CommManager(kernel);
@@ -34,10 +35,25 @@ define(['jupyter-js-widgets'], function(Widgets) {
     };
     WidgetManager.prototype = Object.create(Widgets.ManagerBase.prototype);
 
+    /*
+     * Called when a ipywidget is added to the DOM.
+     *
+     * msg: iopub channel, comm_msg with display data
+     * view: ipywidget view instance
+     * options: object with metadata to be associated with the view
+     */
     WidgetManager.prototype.display_view = function(msg, view, options) {
-        var widgetArea = this._msg_cells[msg.parent_header.msg_id];
+        console.debug('WidgetManager.display view:', msg, view, options);
+
+        var widgetInfo = this._pendingExecutions[msg.parent_header.msg_id];
+        console.debug(widgetInfo);
+
+        view.options = view.options || {};
+        view.options.outputAreaModel = widgetInfo.outputAreaModel;
+
         return Promise.resolve(view).then(function(view) {
-            widgetArea.appendChild(view.el);
+            // display the widget in its assigned DOM node
+            widgetInfo.widgetNode.appendChild(view.el);
             view.on('remove', function() {
                 console.log('view removed', view);
             });
@@ -45,16 +61,73 @@ define(['jupyter-js-widgets'], function(Widgets) {
         });
     };
 
-    WidgetManager.prototype._create_comm = function(targetName, id, metadata) {
-        return Promise.resolve(this.commManager.new_comm(targetName, metadata, id));
+    /*
+     * Returns callbacks to be invoked in response to ipywidget triggered
+     * comm messages on shell, input, and iopub channels. Each callback takes
+     * one parameter: the comm message from the kernel. The return value should
+     * be an object with any of the following properties defined as functions.
+     *
+     * iopub.status
+     * iopub.clear_output
+     * iopub.output
+     * shell.reply
+     * input
+     *
+     * view: ipywidget view instance
+     */
+    WidgetManager.prototype.callbacks = function(view) {
+        // TODO: only registering one callback until https://github.com/ipython/ipywidgets/pull/353
+        // is fixed
+        var mgr = this;
+        var callbacks = {
+            iopub : {
+                output: function(msg) {
+                    console.debug(msg.msg_type, view);
+                    // TODO: it's probably not always parent
+                    mgr.msgHandler(msg, view.options.parent.options.outputAreaModel);
+                }
+            }
+        };
+        return callbacks;
     };
 
+    /*
+     * Called to create a new comm channel between frontend and backend
+     * ipywidget models. Returns a promise of a new comm channel.
+     *
+     * targetName: comm channel target name on the backend
+     * id: ipywidget model ID
+     * metadata: ???
+     */
+    WidgetManager.prototype._create_comm = function(targetName, id, metadata) {
+        return Promise.resolve(
+            this.commManager.new_comm(targetName, {}, {}, metadata, id)
+        );
+    };
+
+    /*
+     * Called to retrieve information about all existing comm channels in order
+     * to initialize frontend comm channel state. Returns a promise of info
+     * about all comms.
+     */
     WidgetManager.prototype._get_comm_info = function() {
         return Promise.resolve({});
     };
 
-    WidgetManager.prototype.addWidget = function(widgetArea, msg_id) {
-        this._msg_cells[msg_id] = widgetArea;
+    /*
+     * Associates a spot in the DOM with a execute_request so that if the
+     * execution results in the creation of a new ipywidget, the ipywidget
+     * can be linked with its output area and place in the DOM.
+     *
+     * msg_id: ID of the execute_request to the kernel
+     * widgetNode: DOM node where the widget should render
+     * outputAreaModel: OutputArea that contains the widget
+     */
+    WidgetManager.prototype.trackPending = function(msg_id, widgetNode, outputAreaModel) {
+        this._pendingExecutions[msg_id] = {
+            widgetNode: widgetNode,
+            outputAreaModel: outputAreaModel
+        };
     };
 
     return WidgetManager;
