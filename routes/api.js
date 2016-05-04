d3ec6caa7114694033ec7be907e68a01b4b4f4bd
James Martin <jameslm@us.ibm.com>
2016-05-04 15:47:19 -0400

    Fix request for Scala kernel
    (c) Copyright IBM Corp. 2016

--- routes/api.js
+++ routes/api.js
@@ -162,13 +162,16 @@ router.post('/kernels', bodyParser.json({ type: 'text/plain' }), function(req, r
           if (notebook.metadata.kernelspec.name){
               var kernelName = notebook.metadata.kernelspec.name;
               debug("Notebook kernel name found: " + kernelName);
+              if (kernelName === "apache_toree"){
+                  kernelName = "scala";
+              }
               req.body.name = kernelName;
           }else{
               // Default to Python 3
               debug("Notebook kernel name not found, defaulting to Python 3");
               req.body.name = "python3";
           }
-          debug(req.body);
+          console.log(req.body);
           // Pass the (modified) request to the kernel gateway.
           request({
             url: urljoin(kgUrl, kgBaseUrl, '/api/kernels'),
